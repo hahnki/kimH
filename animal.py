4d7f40d5a4cead0e305d0825e5189c8b28cf259b
Kimberly Hahn <hahnki@oregonstate.edu>
2020-09-29 18:01:15 -0500

    initial commit

--- animal.py
+++ animal.py
@@ -1,1 +1,5 @@
-
+# Author: Kimberly Hahn
+# Date: 29 Sept 2020
+# Description: Ask the user for their favorite animal and then print out their favorite animal.
+fave_animal = input("Please enter your favorite animal:")
+print("Your favorite animal is the " + fave_animal + ".")
